f99639db4df0e774a6ba925798e32cf15ad1c7cb
Yagnyu Songg <yangyu.song@shopee.com>
2022-07-03 17:10:36 +0800

    support resume download

--- scripts/download_arca23k.sh
+++ scripts/download_arca23k.sh
@@ -7,16 +7,21 @@ command -v unzip >/dev/null 2>&1 || { echo 'unzip is missing' >&2; exit 1; }
 
 mkdir -p _datasets/ARCA23K && cd _datasets/ARCA23K
 
-curl -fOL 'https://zenodo.org/record/5117901/files/ARCA23K.ground_truth.zip'
-curl -fOL 'https://zenodo.org/record/5117901/files/ARCA23K-FSD.ground_truth.zip'
-curl -fOL 'https://zenodo.org/record/5117901/files/ARCA23K.metadata.zip'
-curl -fOL 'https://zenodo.org/record/5117901/files/ARCA23K.audio.z01'
-curl -fOL 'https://zenodo.org/record/5117901/files/ARCA23K.audio.z02'
-curl -fOL 'https://zenodo.org/record/5117901/files/ARCA23K.audio.z03'
-curl -fOL 'https://zenodo.org/record/5117901/files/ARCA23K.audio.z04'
-curl -fOL 'https://zenodo.org/record/5117901/files/ARCA23K.audio.zip'
-curl -fOL 'https://zenodo.org/record/5117901/files/ATTRIBUTION'
-curl -fOL 'https://zenodo.org/record/5117901/files/LICENSE'
+set -x
+set +e
+
+curl -fOL -C - 'https://zenodo.org/record/5117901/files/ARCA23K.ground_truth.zip'
+curl -fOL -C - 'https://zenodo.org/record/5117901/files/ARCA23K-FSD.ground_truth.zip'
+curl -fOL -C - 'https://zenodo.org/record/5117901/files/ARCA23K.metadata.zip'
+curl -fOL -C - 'https://zenodo.org/record/5117901/files/ARCA23K.audio.z01'
+curl -fOL -C - 'https://zenodo.org/record/5117901/files/ARCA23K.audio.z02'
+curl -fOL -C - 'https://zenodo.org/record/5117901/files/ARCA23K.audio.z03'
+curl -fOL -C - 'https://zenodo.org/record/5117901/files/ARCA23K.audio.z04'
+curl -fOL -C - 'https://zenodo.org/record/5117901/files/ARCA23K.audio.zip'
+curl -fOL -C - 'https://zenodo.org/record/5117901/files/ATTRIBUTION'
+curl -fOL -C - 'https://zenodo.org/record/5117901/files/LICENSE'
+
+set -e
 
 unzip 'ARCA23K.ground_truth.zip'
 unzip 'ARCA23K-FSD.ground_truth.zip'
